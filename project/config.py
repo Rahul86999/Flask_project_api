daa74d610350f1e23db8baa3a2acff1cb76249e6
ganeshpawarji <ganesharmy95@gmail.com>
2020-11-17 13:31:33 +0530

    added new flask api using celery

---
+++ project/config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'very_very_secure_and_secret'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+# Flask-Mail
+MAIL_SERVER ='smtp.mailtrap.io'
+MAIL_PORT = 2525
+MAIL_USE_TLS = True
+MAIL_USERNAME = '3d0eddf3f53b72'
+MAIL_PASSWORD = '93a103368550cb'
